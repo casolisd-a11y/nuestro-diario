e1faf2b93e175a7b0372836a12183bef402eb7ae
casolisd-a11y <ca.solisd@alum.up.edu.pe>
2026-01-01 20:22:28 -0500

    Update memories.js

--- memories.js
+++ memories.js
@@ -7,52 +7,52 @@
 
 const memories = [
     {
-        date: "14 de Febrero, 2023",
-        title: "Nuestra Primera Cita",
-        description: "Recuerdo que estaba muy nervioso, pero cuando sonreíste todo se iluminó. Fuimos por ese café helado que tanto te gusta.",
+        date: "09 de Agosto, 2025",
+        title: "Foto en el espejo",
+        description: "Me ha mandado esta foto suya, y no puedo creer lo hermosa que es 😍",
         image: "img/ima1.JPG" // Asegúrate de que el nombre coincida con tu archivo
     },
     {
-        date: "25 de Diciembre, 2023",
-        title: "Primera Navidad Juntos",
-        description: "Ver películas navideñas y cocinar esa cena (aunque se nos quemó un poco) fue lo mejor del año.",
+        date: "11 de Agosto, 2025",
+        title: "Cita en Macchia",
+        description: "Hoy tuvimos una cita en cafetería. Me encanta cuando me cuenta acerca de su forma de ver el mundo, cuando me escucha y su sonrisa 😊",
         image: "img/ima2.jpeg"
     },
       {
-        date: "25 de Diciembre, 2023",
-        title: "Primera Navidad Juntos",
-        description: "Ver películas navideñas y cocinar esa cena (aunque se nos quemó un poco) fue lo mejor del año.",
+        date: "13 de Setiembre, 2025",
+        title: "Noche de teatro",
+        description: "Hoy vinimos hasta Jesús María para presenciar una obra de teatro. Antes pasamos por un matcha y un cold brew de arándano. Te quiero mucho 🤗, espero te esté gustando estos planes de teatro tanto como a mí 🎭",
         image: "img/ima3.jpeg"
     },
       {
-        date: "25 de Diciembre, 2023",
-        title: "Primera Navidad Juntos",
-        description: "Ver películas navideñas y cocinar esa cena (aunque se nos quemó un poco) fue lo mejor del año.",
+        date: "18 de Octubre, 2025",
+        title: "Tarde de playa, comida y películas 🏖️",
+        description: "La reina tuvo la idea de ver Crepúsculo mientras comíamos frente al mar. Me encantó todo. Me encanta ella, la quiero muchísimo y a veces creo que me preocupo tanto de cómo esté que me da muchas ansias. Gracias por todo lo que haces por nosotros princesa ❤️👸. Te quiero mucho ❤️",
         image: "img/ima4.jpeg"
     },
       {
-        date: "25 de Diciembre, 2023",
-        title: "Primera Navidad Juntos",
-        description: "Ver películas navideñas y cocinar esa cena (aunque se nos quemó un poco) fue lo mejor del año.",
+        date: "1 de Noviembre, 2025",
+        title: "Cita y pedida a la princesa",
+        description: "Estoy escribiendo esto muy temprano, aún no tengo la foto pero sí todo preparado y estoy muy nervioso. Lo que siento por la princesa es algo muy único; y lo que más deseo es impulsarla con sus objetivos porque ella me anima a mí a ser una mejor persona. Espero que le guste lo preparado, ella me contó que aprecia mucho el detalle en la gastronomía, y le preparé esta sorpresa. [por la noche] Ya tengo la foto, y tengo de enamorada a mi princesa ❤️❤️❤️. No puedo creerlo. Muchas gracias por todas las alegrías que nos das reina. Me haces inmensamente feliz y siempre estaré para ti, te admiro muchísimo ❤️❤️❤️. Y sueño mucho contigo y con nuestras hijas ❤️.",
         image: "img/ima5.jpeg"
     },
       {
-        date: "25 de Diciembre, 2023",
-        title: "Primera Navidad Juntos",
-        description: "Ver películas navideñas y cocinar esa cena (aunque se nos quemó un poco) fue lo mejor del año.",
+        date: "21 de Noviembre, 2025",
+        title: "Noche de tacos y la familia de mi amor",
+        description: "Hoy tuvimos una noche de tacos en la casa de la princesa. Estuve super nervioso por conocer a su familia. Espero que les haya caído bien, ya le preguntaré. Muchas gracias por esto reinita, ya quiero que conozcas a mis papás ❤️❤️❤️",
         image: "img/ima6.jpeg"
     },
       {
-        date: "25 de Diciembre, 2023",
-        title: "Primera Navidad Juntos",
-        description: "Ver películas navideñas y cocinar esa cena (aunque se nos quemó un poco) fue lo mejor del año.",
+        date: "22 de Noviembre, 2025",
+        title: "Foto bellísima y salida al Jockey",
+        description: "El día de ayer estuve con mi amorcito, y fuimos a comprar su nuevo celular al Jockey 📱, el suyo ya estaba un poco lento. También comimos postre en Alanya, lo que me puso sentimental porque recordé cuando ella me invitó la primera vez 😍. Vimos un pez globo 🐡. Hoy la princesa me mandó esta foto que la amo, me encanta, y la amo a ella ❤️. Te amo mucho mi amor ❤️, gracias por los momentos y por hacernos tan felices ❤️",
         image: "img/ima7.jpeg"
     },
     // Copia desde aquí para agregar otro...
     {
-        date: "AÑO NUEVO 2024",
-        title: "Viaje a la Playa",
-        description: "El atardecer fue increíble, pero verte feliz fue aún mejor.",
+        date: "30 de Noviembre, 2025",
+        title: "Toffees",
+        description: "He comprado toffees  para la familia de mi amorcito. Espero les guste mucho. Volveré a estudiar.",
         image: "img/ima8.jpeg" // Puedes usar links de internet también
     }
     // ... hasta aquí
